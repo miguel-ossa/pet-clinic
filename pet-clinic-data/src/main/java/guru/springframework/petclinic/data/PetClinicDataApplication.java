4a0f36aac3b014d9f03aa6faa8cb85478a69e964
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2020-09-12 18:13:33 +0100

    Refactoring to multi-module project. Closes #1.

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/data/PetClinicDataApplication.java
@@ -0,0 +1,10 @@
+package guru.springframework.petclinic.data;
+
+import org.springframework.boot.SpringApplication;
+
+public class PetClinicDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicDataApplication.class, args);
+	}
+}

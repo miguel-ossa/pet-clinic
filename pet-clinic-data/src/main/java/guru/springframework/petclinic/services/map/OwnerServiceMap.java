85387af43127e46ab2f0744636cec014252e4650
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2020-09-14 14:45:38 +0100

    Implementation of based services. Closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,46 @@
+package guru.springframework.petclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.petclinic.model.Owner;
+import guru.springframework.petclinic.services.CrudService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+	@Override
+	public Set<Owner> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Owner save(Long id, Owner object) {
+		// TODO Auto-generated method stub
+		return super.save(id, object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		// TODO Auto-generated method stub
+		return super.save(object.getId(), object);
+	}
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,46 @@
+package guru.springframework.petclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.petclinic.model.Pet;
+import guru.springframework.petclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+	@Override
+	public Pet save(Pet object) {
+		// TODO Auto-generated method stub
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public Set<Pet> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Long id, Pet object) {
+		// TODO Auto-generated method stub
+		return super.save(id, object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+}

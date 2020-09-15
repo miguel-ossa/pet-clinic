369f173fb08ff6e83d24e1fde29b584b8a145d65
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2020-09-15 18:17:23 +0100

    Added Visit and Owner relationship with Pet. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -1,10 +1,22 @@
 package guru.springframework.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6168114871598177503L;
+
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }

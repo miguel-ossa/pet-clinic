550a1acb803a8d3772f8e03161fcf0be431e79ad
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2020-09-17 19:01:41 +0100

    Added Spring JPA service for PetTypes. Closes #42

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,49 @@
+package guru.springframework.petclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import guru.springframework.petclinic.model.Specialty;
+import guru.springframework.petclinic.repositories.SpecialtyRepository;
+import guru.springframework.petclinic.services.SpecialtyService;
+
+public class SpecialtySDJpaService implements SpecialtyService {
+
+	private final SpecialtyRepository specialtyRepository;
+	
+	public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+		super();
+		this.specialtyRepository = specialtyRepository;
+	}
+
+	@Override
+	public Set<Specialty> findAll() {
+		
+		Set<Specialty> specialties = new HashSet<>();
+		
+		specialtyRepository.findAll().forEach(specialties::add);
+		
+		return specialties;
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return specialtyRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Specialty save(Specialty object) {
+		return specialtyRepository.save(object);
+	}
+
+	@Override
+	public void delete(Specialty object) {
+		specialtyRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialtyRepository.deleteById(id);
+	}
+
+}

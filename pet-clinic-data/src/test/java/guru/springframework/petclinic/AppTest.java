6344301e6d9e4e49b6de13e25f9010c9826a2ccf
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2020-10-19 19:23:40 +0100

    Upgraded to JUnit 5. Closes #4

--- pet-clinic-data/src/test/java/guru/springframework/petclinic/AppTest.java
+++ pet-clinic-data/src/test/java/guru/springframework/petclinic/AppTest.java
@@ -1,37 +1,15 @@
 package guru.springframework.petclinic;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-public class AppTest 
-extends TestCase
-{
-/**
- * Create the test case
- *
- * @param testName name of the test case
- */
-public AppTest( String testName )
-{
-    super( testName );
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class AppTest {
+	@Test
+	public void contextLoads() {
+
+	}
 }
-
-/**
- * @return the suite of tests being tested
- */
-public static Test suite()
-{
-    return new TestSuite( AppTest.class );
-}
-
-/**
- * Rigourous Test :-)
- */
-public void testApp()
-{
-    assertTrue( true );
-}
-}
-
-
--- pet-clinic-web/src/test/java/guru/springframework/petclinic/web/AppTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/web/AppTest.java
@@ -1,38 +1,19 @@
 package guru.springframework.petclinic.web;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+	public void contextLoads() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	}
 }
